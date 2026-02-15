208814da9b1341b6410843f3c0d0e4c6097008d4
Alexis <pinfalldatal@gmail.com>
2026-02-15 19:21:43 +0100

    config: allow supabase and postimg images

--- next.config.js
+++ next.config.js
@@ -6,10 +6,22 @@ const withNextIntl = createNextIntlPlugin();
 const nextConfig = {
   images: {
     remotePatterns: [
+      // 1. Ton stockage Supabase (Specifique à ton projet)
       {
         protocol: 'https',
-        hostname: '*.supabase.co',
-        pathname: '/storage/v1/object/public/**',
+        hostname: 'xusywypjmogzbizrwruv.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/**', // Autorise toutes les images dans le dossier public
+      },
+      // 2. Postimg (Liens directs - le plus important)
+      {
+        protocol: 'https',
+        hostname: 'i.postimg.cc',
+      },
+      // 3. Postimg (Site web - au cas où)
+      {
+        protocol: 'https',
+        hostname: 'postimg.cc',
       },
     ],
   },
